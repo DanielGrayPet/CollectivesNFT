46c4b7a24fecc400274a6b6d17f1c31dc8c14d9f
Daniel <petrusx.px@gmail.com>
2022-05-08 17:47:20 +0300

    Added Layout and CSS

---
+++ src/components/Layout.js
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import { container, heading, navLinks, navLinkItem, navLinkText } from './Layout.module.css'
+
+const Layout = ({pageTitle}) => {
+    return (
+        <div className={container}>
+            <main>
+                <h1 className={heading}>Collectives</h1>
+            </main>
+            <title>{pageTitle}</title>
+            <nav>
+                <ul className={navLinks}>
+                    <li className={navLinkItem}><Link to = "/" className={navLinkText}>Home</Link></li>
+                    <li className={navLinkItem}><Link to = "/gallery" className={navLinkText}>Gallery</Link></li>
+                    <li className={navLinkItem}><Link to = "/about" className={navLinkText}>About</Link></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Layout
---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+import Layout from "../components/Layout"
+
+const aboutpage = () => {
+  return(
+    <Layout pageTitle="About"></Layout>
+  )
+}
+
+export default aboutpage
